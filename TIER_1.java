20e4494a4f06ac4b5a62d89a5b634fa3915473da
TR <theresa.reschauer@gymnasium-beilnries.de>
2020-06-29 15:54:20 +0200

    Initial sharing of project

---
+++ TIER_1.java
@@ -0,0 +1,33 @@
+
+/**
+ *Tierversuch
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class TIER_1
+{
+    // instance variables - replace the example below with your own
+    private int x;
+
+    /**
+     * Constructor for objects of class TIER_1
+     */
+    public TIER_1()
+    {
+        // initialise instance variables
+        x = 0;
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    public int sampleMethod(int y)
+    {
+        // put your code here
+        return x + y;
+    }
+}
